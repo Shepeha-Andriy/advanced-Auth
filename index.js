735d9228c1e70f865bda24e7bf436fcb5439ade3
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-05-27 18:41:32 +0300

    test

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL
-}));
+app.use(cors());
 app.use('/api', router);
 app.use(errorMiddleware);
 
